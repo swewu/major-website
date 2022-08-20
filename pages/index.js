ab5bbb96f216f5729897c9bde92cee0af15dbd52
protal <slipknot.opal@gmail.com>
2022-08-20 23:09:21 +0700

    feat: add welcome image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      <h1> Welcome to Software Engineer</h1>
+      <Image
+        src="/asset/welcome.jpg"
+        alt="welcome"
+        width="640px"
+        height="420px"
+        layout="fixed"
+      ></Image>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
