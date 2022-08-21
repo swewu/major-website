de93a689b30aaeeb2c44071ad7226d5b0a35ef55
bbellsz <sirada.bn@mail.wu.ac.th>
2022-08-21 11:56:14 +0700

    4

--- pages/4.js
+++ pages/4.js
@@ -1,5 +1,30 @@
 function FourPage() {
-  return <div>four</div>;
+  return <div>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/114744261_2778066129097411_4062713816018508267_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHIt9aZJAqFuoMGVdp7j4jCkBUZYtvxXWeQFRli2_FdZ0NdCR8GnUmbie0wCPW4Xf58NLwHZgEPFmrZahY9_14e&_nc_ohc=plRzGBWLWGQAX_fP40E&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT_rw4_YFwi-Ru0mZuDP6aYJzMH09nrNAoZAq-2PthHnHw&oe=63276E94" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/69150284_866589813741725_3058136840692301824_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeGT_vRtY0httzsF1hR4HOYB3--eSKa8OATf755Iprw4BAkV5ekDaCaeCrg4NFMQeRBBbVoVFEuEDWfKCya2oeG3&_nc_ohc=Bmge9vZwRGsAX802Fgk&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT99MMA0aVX0PKTdxMy_hOXbP5bwPqqOPLb7mY1Bd7kdUg&oe=63255304" className="d-block w-100"/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/67265333_842484962818877_2315058656116211712_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeGWwzSywE0ejXFu-B7q_sdbbP4bBhlrN31s_hsGGWs3fVLrYrgIwK1HQbaLhbta2OiT7TsJA2RLlkL1vj8WAlqg&_nc_ohc=ZdNLzOpGsKUAX8Jymsw&_nc_oc=AQkLD8jsS81CVdCV6NtXMNSa3eiHCOl_wR0fACFbRGpsY8is1hs8L9zZVaZMx2R9VBM&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT9rf94yPjUSpnxMhjzzKPMFhdV_oxW0jAXD7xKpC-1phg&oe=632730D5" className="d-block w-100" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+
+</div>
 }
 
 export default FourPage;
