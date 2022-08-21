2a187592fd260b12b4b1469e31dbaf6ded0d21d7
bbellsz <sirada.bn@mail.wu.ac.th>
2022-08-21 13:47:01 +0700

    change template

--- pages/4.js
+++ pages/4.js
@@ -1,30 +1,41 @@
 function FourPage() {
   return <div>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-6 col-sm-6">
+          <img src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1"
+            width="150" height="70" />
+        </div>
+        <div className="col-6 col-sm-6">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดกิจกรรมในหัวข้อ Software Testing in Real World</div>
 
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/114744261_2778066129097411_4062713816018508267_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHIt9aZJAqFuoMGVdp7j4jCkBUZYtvxXWeQFRli2_FdZ0NdCR8GnUmbie0wCPW4Xf58NLwHZgEPFmrZahY9_14e&_nc_ohc=plRzGBWLWGQAX_fP40E&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT_rw4_YFwi-Ru0mZuDP6aYJzMH09nrNAoZAq-2PthHnHw&oe=63276E94" class="d-block w-100" alt="..." />
-    </div>
-    <div class="carousel-item">
-      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/69150284_866589813741725_3058136840692301824_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeGT_vRtY0httzsF1hR4HOYB3--eSKa8OATf755Iprw4BAkV5ekDaCaeCrg4NFMQeRBBbVoVFEuEDWfKCya2oeG3&_nc_ohc=Bmge9vZwRGsAX802Fgk&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT99MMA0aVX0PKTdxMy_hOXbP5bwPqqOPLb7mY1Bd7kdUg&oe=63255304" className="d-block w-100"/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/67265333_842484962818877_2315058656116211712_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeGWwzSywE0ejXFu-B7q_sdbbP4bBhlrN31s_hsGGWs3fVLrYrgIwK1HQbaLhbta2OiT7TsJA2RLlkL1vj8WAlqg&_nc_ohc=ZdNLzOpGsKUAX8Jymsw&_nc_oc=AQkLD8jsS81CVdCV6NtXMNSa3eiHCOl_wR0fACFbRGpsY8is1hs8L9zZVaZMx2R9VBM&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT9rf94yPjUSpnxMhjzzKPMFhdV_oxW0jAXD7xKpC-1phg&oe=632730D5" className="d-block w-100" class="d-block w-100" alt="..."/>
+        <div className="w-100"></div>
+
+        <div className="col-6 col-sm-6">
+          <img src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1"
+            width="150" height="70" />
+        </div>
+        <div className="col-6 col-sm-6">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</div>
+
+        <div className="w-100"></div>
+
+        <div className="col-6 col-sm-6">
+          <img src="https://informatics.wu.ac.th/wp-content/uploads/113211861_2778064455764245_6364607256678117533_n.jpg"
+            width="150" height="70" />
+        </div>
+        <div className="col-6 col-sm-6">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ รับเสื้อช็อปมอบโดยประธานหลักสูตร</div>
+
+        <div className="w-100"></div>
+
+        <div className="col-6 col-sm-6">
+          <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/68852926_866589713741735_5111548477138010112_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHkklxPKQ1CaSsokWZYXPFAYPIM0lRP82Vg8gzSVE_zZaEsAWn6JyiXSSzZcPtaSAp7mbaF1_AJCxwFvPt9BDtz&_nc_ohc=N4IRiTQcrdoAX8PMlGM&tn=e88y0UvF3XutaiB7&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT-dC59Pd2Z04GjOxerxJ8hIBZfIUX2R-65X397QQH6j1w&oe=63284286"
+            width="150" height="70" />
+        </div>
+        <div className="col-6 col-sm-6">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ้เข้าร่วมกิจกรรมประกวดดาว-เดือน สำนักวิชา สารสนเทศศาสตร์</div>
+
+
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    
-
-</div>
 }
 
 export default FourPage;
