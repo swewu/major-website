2a2673d5f59b4683dc7c74b505ed3e10a5511928
bbellsz <sirada.bn@mail.wu.ac.th>
2022-08-21 15:19:10 +0700

    last version

--- pages/4.js
+++ pages/4.js
@@ -9,18 +9,18 @@ function FourPage() {
 
   }
   return <div>
-    <h1 className="text-center " style={{ color: "indigo" }}>Software Engineering Activities</h1>
+    <h1 className="text-center fw-bold" style={{ color: "indigo" }}>Software Engineering Activities</h1>
     <hr />
     <div className="container py-5 ">
       <div className="row">
         <div className="col-lg-12">
           <div className="row">
             <div className="col-md-4 ">
-              <div className="card mb-4 product-wap rounded-0 hover-zoom " >
+              <div className="card mb-4 product-wap rounded-0 " >
                 <div className="card rounded-0 ">
                   <img className="card-img rounded-0 img-fluid" style={style1} src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1" />
                 </div>
-                <div className="card-body">
+                <div className="card-body btn btn-outline-secondary">
                   <p className="text-left mb-0 fw-bold" style={style2}>กิจกรรมในหัวข้อ Software Testing in Real World</p>
                 </div>
               </div>
@@ -30,9 +30,8 @@ function FourPage() {
                 <div className="card rounded-0">
                   <img className="card-img rounded-0 img-fluid imggg " style={style1} src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/08/117342044_602149667110733_7898724889523486621_n.jpg?resize=1536%2C694&ssl=1" />
                 </div>
-                <div className="card-body">
+                <div className="card-body btn btn-outline-secondary">
                   <p className="text-left mb-0 fw-bold" style={style2}>กิจกรรมโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</p>
-                  <p></p>
                 </div>
               </div>
             </div>
@@ -41,7 +40,7 @@ function FourPage() {
                 <div className="card rounded-0">
                   <img className="card-img rounded-0 img-fluid" style={style1} src="https://informatics.wu.ac.th/wp-content/uploads/113211861_2778064455764245_6364607256678117533_n.jpg" />
                 </div>
-                <div className="card-body">
+                <div className="card-body btn btn-outline-secondary">
                   <p className="text-left mb-0 fw-bold" style={style2}>นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ รับเสื้อช็อปมอบโดยประธานหลักสูตร</p>
                 </div>
               </div>
@@ -51,7 +50,7 @@ function FourPage() {
                 <div className="card rounded-0">
                   <img className="card-img rounded-0 img-fluid" style={style1} src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/68852926_866589713741735_5111548477138010112_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHkklxPKQ1CaSsokWZYXPFAYPIM0lRP82Vg8gzSVE_zZaEsAWn6JyiXSSzZcPtaSAp7mbaF1_AJCxwFvPt9BDtz&_nc_ohc=N4IRiTQcrdoAX8PMlGM&tn=e88y0UvF3XutaiB7&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT-dC59Pd2Z04GjOxerxJ8hIBZfIUX2R-65X397QQH6j1w&oe=63284286" />
                 </div>
-                <div className="card-body">
+                <div className="card-body btn btn-outline-secondary">
                   <p className="text-left mb-0 fw-bold" style={style2}>นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ้เข้าร่วมกิจกรรมประกวดดาว-เดือน สำนักวิชา สารสนเทศศาสตร์</p>
                 </div>
               </div>
@@ -61,7 +60,7 @@ function FourPage() {
                 <div className="card rounded-0">
                   <img className="card-img rounded-0 img-fluid" style={style1} src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1" />
                 </div>
-                <div className="card-body">
+                <div className="card-body btn btn-outline-secondary">
                   <p className="text-left mb-0 fw-bold" style={style2}>หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</p>
                 </div>
               </div>
@@ -71,8 +70,8 @@ function FourPage() {
                 <div className="card rounded-0">
                   <img className="card-img rounded-0 img-fluid" style={style1} src="https://scontent.fbkk2-4.fna.fbcdn.net/v/t1.6435-9/68858709_2412628062149898_7694711541764980736_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeF_zAHJSlc_Mwl-iRULfdYHCDr05lW88pUIOvTmVbzylezJMp2fJU-770rdRElpwkMVg3lSk6aY_lH6QvfW3kq2&_nc_ohc=txDXMRG5zqEAX--YdRE&_nc_ht=scontent.fbkk2-4.fna&oh=00_AT9VrxM5Up4AwHPknh-fyqNJT30oQK79ki07FCRQ1t9Lmw&oe=63277F4C" />
                 </div>
-                <div className="card-body">
-                  <p className="text-left mb-0 fw-bold" style={style2}>หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ กิจกรรมดาวเดือน คณะสารสนเทศศาสตร์</p>
+                <div className="card-body btn btn-outline-secondary">
+                  <p className="text-left mb-0 fw-bold" style={style2}>หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ กิจกรรมดาวเดือน ประจำปี 2562 สำนักวิชาสารสนเทศศาสตร์</p>
                 </div>
               </div>
             </div>
@@ -82,7 +81,7 @@ function FourPage() {
 
       </div>
     </div>
-  </div>
+  </div >
 }
 
 export default FourPage;
