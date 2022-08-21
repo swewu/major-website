1bad64140e3486e957373543e83d501e67adfcd8
bbellsz <sirada.bn@mail.wu.ac.th>
2022-08-21 13:58:47 +0700

    pic

--- pages/4.js
+++ pages/4.js
@@ -1,37 +1,63 @@
 function FourPage() {
   return <div>
-    <div className="container text-center">
+
+    <div className="container py-5">
       <div className="row">
-        <div className="col-6 col-sm-6">
-          <img src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1"
-            width="150" height="70" />
+        <div className="col-lg-12">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img className="card-img rounded-0 img-fluid" src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1" />
+                </div>
+                <div className="card-body">
+                  <p className="text-left mb-0">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดกิจกรรมในหัวข้อ Software Testing in Real World</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img className="card-img rounded-0 img-fluid" src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/08/117342044_602149667110733_7898724889523486621_n.jpg?resize=1536%2C694&ssl=1" />
+                </div>
+                <div className="card-body">
+                  <p className="text-left mb-0">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img className="card-img rounded-0 img-fluid" src="https://informatics.wu.ac.th/wp-content/uploads/113211861_2778064455764245_6364607256678117533_n.jpg" />
+                </div>
+                <div className="card-body">
+                  <p className="text-left mb-0">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ รับเสื้อช็อปมอบโดยประธานหลักสูตร</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img className="card-img rounded-0 img-fluid" src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/68852926_866589713741735_5111548477138010112_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHkklxPKQ1CaSsokWZYXPFAYPIM0lRP82Vg8gzSVE_zZaEsAWn6JyiXSSzZcPtaSAp7mbaF1_AJCxwFvPt9BDtz&_nc_ohc=N4IRiTQcrdoAX8PMlGM&tn=e88y0UvF3XutaiB7&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT-dC59Pd2Z04GjOxerxJ8hIBZfIUX2R-65X397QQH6j1w&oe=63284286" />
+                </div>
+                <div className="card-body">
+                  <p className="text-left mb-0">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ้เข้าร่วมกิจกรรมประกวดดาว-เดือน สำนักวิชา สารสนเทศศาสตร์</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img className="card-img rounded-0 img-fluid" src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1" />
+                </div>
+                <div className="card-body">
+                  <p className="text-left mb-0">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-        <div className="col-6 col-sm-6">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดกิจกรรมในหัวข้อ Software Testing in Real World</div>
-
-        <div className="w-100"></div>
-
-        <div className="col-6 col-sm-6">
-          <img src="https://i0.wp.com/engineer.wu.ac.th/wp-content/uploads/2020/09/118615927_3266703680076595_7050269934999517531_o.jpg?fit=1024%2C683&ssl=1"
-            width="150" height="70" />
-        </div>
-        <div className="col-6 col-sm-6">หลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ จัดโครงการค่ายอบรมเชิงปฏิบัติการกระบวนการซอฟต์แวร์ แบบ SCRUM</div>
-
-        <div className="w-100"></div>
-
-        <div className="col-6 col-sm-6">
-          <img src="https://informatics.wu.ac.th/wp-content/uploads/113211861_2778064455764245_6364607256678117533_n.jpg"
-            width="150" height="70" />
-        </div>
-        <div className="col-6 col-sm-6">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ม.วลัยลักษณ์ รับเสื้อช็อปมอบโดยประธานหลักสูตร</div>
-
-        <div className="w-100"></div>
-
-        <div className="col-6 col-sm-6">
-          <img src="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.6435-9/68852926_866589713741735_5111548477138010112_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=b9115d&_nc_eui2=AeHkklxPKQ1CaSsokWZYXPFAYPIM0lRP82Vg8gzSVE_zZaEsAWn6JyiXSSzZcPtaSAp7mbaF1_AJCxwFvPt9BDtz&_nc_ohc=N4IRiTQcrdoAX8PMlGM&tn=e88y0UvF3XutaiB7&_nc_ht=scontent.fbkk20-1.fna&oh=00_AT-dC59Pd2Z04GjOxerxJ8hIBZfIUX2R-65X397QQH6j1w&oe=63284286"
-            width="150" height="70" />
-        </div>
-        <div className="col-6 col-sm-6">นักศึกษาหลักสูตรวิศวกรรมซอฟต์แวร์ ้เข้าร่วมกิจกรรมประกวดดาว-เดือน สำนักวิชา สารสนเทศศาสตร์</div>
-
 
       </div>
     </div>
